754c38aa4f026b55376fcfbfa2ecfff729905a7b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-11-26 15:04:45 +0100

    add decoder/encoder structs, enum, basic structures and types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
+enum RespValue {
+    SimpleString(String),
+    Error(String),
+    Integer(i64),
+    BulkString(Vec<u8>), // raw bytes, may not be UTF-8
+    Array(Vec<RespValue>),
+}
+
+struct Decoder {
+    buf: [u8; 512],
+    offset: usize,
+}
+
+impl Decoder {
+    fn parse_value(&mut self) -> RespValue {
+        match self.buf[self.offset] {
+            // b'*' => self.parse_array(),
+            // b'+' => self.parse_simple_string(),
+            // b'$' => self.parse_bulk_string(),
+            // b':' => self.parse_integer(),
+            // b'-' => self.parse_error(),
+            _ => panic!("unknown RESP type"),
+        }
+    }
+    fn parse_array(&mut self) {}
+}
+
+fn handle_stream(stream: &mut TcpStream) {
+    let mut buf: [u8; 512] = [0; 512];
+    let (bytes) = stream.read(&mut buf);
+}
 fn main() {
     println!("Logs from your program will appear here!");
 
@@ -8,9 +42,7 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(mut stream) => handle_stream(&mut stream),
             Err(e) => {
                 println!("error: {}", e);
             }
